29069058f1defb4267a43b2552f8f743a2bca33e
sachsmc <sachsmc@gmail.com>
2018-03-12 09:54:50 +0100

    multivariate X

--- R/generate_data.R
+++ R/generate_data.R
@@ -4,34 +4,48 @@
 #' some linear, some wonky associations, an interaction, and some variables correlated.
 #'
 #' @param n Sample size
+#' @param scenario Character designating the simulation scenario: 0 = NULL, no association, A = simple, one variable
+#'                 is linearly associated with IRIS,
+#'                 B = simple multivariate, 5 variables are linearly associated,
+#'                 C = wonky, interactions, nonlinearities, etc.
 #'
 #' @return A data frame with X variables, censored survival times (competing risk w death), and true cumulative incidence at 26.5 weeks
 #'
 #' @export
 
 
-generate_data <- function(n = 500) {
+generate_data <- function(n = 500, scenario = "A") {
 
-  ## TODO generate multiple x values
-  X <- rnorm(n, mean = 1, sd = .1)
+  X5 <- matrix(rnorm(n * 5, mean = 1, sd = .1), ncol = 5)
+  X52 <- X5 %*% matrix(.25, nrow = 5, ncol = 5) + matrix(rnorm(n * 5, mean = 1, sd = .1), ncol = 5)
+  X53 <- X52 %*% matrix(.1, nrow = 5, ncol = 5) + matrix(rnorm(n * 5, mean = 1, sd = .5), ncol = 5)
+  X54 <- X53 %*% matrix(.05, nrow = 5, ncol = 5) + matrix(rnorm(n * 5, mean = 1, sd = .65), ncol = 5)
 
-  g2 <- exp(.25 + .1 * X + rnorm(n, sd = .1))
-  g1 <- exp(2 * X + rnorm(n, sd = .01))
+  X <- cbind(X5, X52, X53, X54)
 
-  k2 <- exp(.05 + .025 * X + rnorm(n, sd = .1))
-  k1 <- exp(.5 * X + rnorm(n, sd = .01))
+  if(scenario == "0") {
 
+    g2 <- exp(.25 + rnorm(n, sd = .1))
+    g1 <- exp(rnorm(n, sd = .01))
 
-  # ensure that g1 satisfies pweibull(26.5) = .20
-  # k1 satisfies pweibull(26.5) = .07
+  } else if(scenario == "A") {
+
+    g2 <- exp(.25 + .1 * X[, 1] + rnorm(n, sd = .1))
+    g1 <- exp(2 * X[, 1] + rnorm(n, sd = .01))
+
+  }
+
+  k2 <- exp(.05 + .025 * X[, 1] + rnorm(n, sd = .1))
+  k1 <- exp(.5 * X[, 1] + rnorm(n, sd = .01))
+
+  # ensure that g1 satisfies pweibull(26.5) ~= .20
+  # ensure that k1 satisfies pweibull(26.5) ~= .07
 
   rescl <- mean(26.5 / ((-log(.8)) ^ (1/g2)))
   reskl <- mean(26.5 / ((-log(1 - .07)) ^ (1 / k2)))
 
   g1 <- g1 * rescl / mean(g1)
   k1 <- k1 * reskl / mean(k1)
-
-  ## TODO generate competing risk
 
   Y <- rweibull(n, scale = g1, shape = g2)
   Y2 <- rweibull(n, scale = k1, shape = k2)
--- tests/testthat/test_generate.R
+++ tests/testthat/test_generate.R
@@ -2,9 +2,13 @@ context("Data generation")
 
 testthat("Generated data has approximately 20% cumulative incidence at 26.5 weeks", {
 
-  ckdat <- generate_data(n = 15000)
+  ckdat <- generate_data(n = 15000, "A")
   expect_equal(mean(ckdat$trueP), .2, tolerance = .05)
   expect_equal(1 - summary(survfit(Surv(Tout, delta == 1) ~ 1, data = ckdat), times = 26.5)$surv, .2, tolerance = .05)
+
+  ckdat <- generate_data(n = 15000, "0")
+  expect_equal(mean(ckdat$trueP), .2, tolerance = .05)
+
 
   ckdat <- generate_data(n = 2000)
   psuo <- pseudoci(ckdat$Tout, event = ckdat$delta, tmax = 26.5)

fb4ddc092d2848768ca3f857142ebd583285b7f1
sachsmc <sachsmc@gmail.com>
2018-03-12 09:38:15 +0100

    competing risks

--- R/generate_data.R
+++ R/generate_data.R
@@ -18,25 +18,34 @@ generate_data <- function(n = 500) {
   g2 <- exp(.25 + .1 * X + rnorm(n, sd = .1))
   g1 <- exp(2 * X + rnorm(n, sd = .01))
 
+  k2 <- exp(.05 + .025 * X + rnorm(n, sd = .1))
+  k1 <- exp(.5 * X + rnorm(n, sd = .01))
+
+
   # ensure that g1 satisfies pweibull(26.5) = .20
+  # k1 satisfies pweibull(26.5) = .07
 
   rescl <- mean(26.5 / ((-log(.8)) ^ (1/g2)))
+  reskl <- mean(26.5 / ((-log(1 - .07)) ^ (1 / k2)))
 
   g1 <- g1 * rescl / mean(g1)
+  k1 <- k1 * reskl / mean(k1)
 
   ## TODO generate competing risk
 
   Y <- rweibull(n, scale = g1, shape = g2)
+  Y2 <- rweibull(n, scale = k1, shape = k2)
 
   Cen <- pmin(runif(n, 0, 50), 50)
 
-  Tout <- pmin(Y, Cen)
-  delta <- Y < Cen
+  Tout <- pmin(Y, Y2, Cen)
+  delta <- ifelse(Cen < Y & Cen < Y2, 0,
+                  ifelse(Y < Y2, 1, 2))
 
   trueP <- pweibull(26.5, scale = g1, shape = g2)
 
   data.frame(Tout, delta, X, trueT = Y < 26.5,
-             trueP)
+             trueP, Cen, Y, Y2)
 
 
 
--- tests/testthat/test_generate.R
+++ tests/testthat/test_generate.R
@@ -1,9 +1,14 @@
 context("Data generation")
 
-testthat("Generated data has approximately 25% cumulative incidence at 26.5 weeks", {
+testthat("Generated data has approximately 20% cumulative incidence at 26.5 weeks", {
 
   ckdat <- generate_data(n = 15000)
   expect_equal(mean(ckdat$trueP), .2, tolerance = .05)
-  expect_equal(1 - summary(survfit(Surv(Tout, delta) ~ 1, data = ckdat), times = 26.5)$surv, .2, tolerance = .05)
+  expect_equal(1 - summary(survfit(Surv(Tout, delta == 1) ~ 1, data = ckdat), times = 26.5)$surv, .2, tolerance = .05)
+
+  ckdat <- generate_data(n = 2000)
+  psuo <- pseudoci(ckdat$Tout, event = ckdat$delta, tmax = 26.5)
+  expect_equal(mean(psuo$pseudo$cause1[, 1]), .2, tolerance = .05)
+  expect_equal(mean(psuo$pseudo$cause2[, 1]), .07, tolerance = .025)
 
 })
